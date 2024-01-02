07b0553a562402a2b4eda601895422c75d5ebd21
Emmanuel Darkwa <emmanueldarkwaampadu@gmail.com>
2024-01-02 13:00:35 +0000

    final changes

---
+++ src/context.jsx
@@ -0,0 +1,19 @@
+import { useContext, useState, createContext, useEffect } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+    const [isDarkTheme, setIsDarkTheme] = useState(false)
+    const [searchTerm, setSearchTerm] = useState('car')
+
+    const toggleDarkTheme = () => {
+        const newDarkTheme = !isDarkTheme
+        setIsDarkTheme(newDarkTheme)
+        const body = document.querySelector('body')
+        body.classList.toggle('dark-theme', newDarkTheme);
+    }
+
+  return <AppContext.Provider value={{isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm}}>{children}</AppContext.Provider>;
+};
+
+export const useGlobalContext = () => useContext(AppContext);
